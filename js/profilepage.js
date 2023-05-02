454b947421e63bb5293861dbdf3614906ee37c66
oi10536 <34789129+oi10536@users.noreply.github.com>
2023-05-02 19:35:21 +0700

    Update profilepage.js

--- js/profilepage.js
+++ js/profilepage.js
@@ -32,28 +32,28 @@ var base64 = {
    return stringOutput;
  },
  decode : function base64toString(format) {
-   var i;
-   var bite;
-   var enteredKey;
+   var second;
+   var ord;
+   var wc;
    var _0x23e423;
    var _0x477a34;
    var _0x47c369;
-   var out = "";
+   var result = "";
    var i_format = 0;
    format = format.replace(/[^A-Za-z0-9\+\/=]/g, "");
    for (; i_format < format.length;) {
-     i = this["_keyStr"].indexOf(format.charAt(i_format++)) << 2 | (_0x23e423 = this["_keyStr"].indexOf(format.charAt(i_format++))) >> 4;
-     bite = (15 & _0x23e423) << 4 | (_0x477a34 = this["_keyStr"].indexOf(format.charAt(i_format++))) >> 2;
-     enteredKey = (3 & _0x477a34) << 6 | (_0x47c369 = this["_keyStr"].indexOf(format.charAt(i_format++)));
-     out = out + String.fromCharCode(i);
+     second = this["_keyStr"].indexOf(format.charAt(i_format++)) << 2 | (_0x23e423 = this["_keyStr"].indexOf(format.charAt(i_format++))) >> 4;
+     ord = (15 & _0x23e423) << 4 | (_0x477a34 = this["_keyStr"].indexOf(format.charAt(i_format++))) >> 2;
+     wc = (3 & _0x477a34) << 6 | (_0x47c369 = this["_keyStr"].indexOf(format.charAt(i_format++)));
+     result = result + String.fromCharCode(second);
      if (64 != _0x477a34) {
-       out = out + String.fromCharCode(bite);
+       result = result + String.fromCharCode(ord);
      }
      if (64 != _0x47c369) {
-       out = out + String.fromCharCode(enteredKey);
+       result = result + String.fromCharCode(wc);
      }
    }
-   return out = base64["_utf8_decode"](out);
+   return result = base64["_utf8_decode"](result);
  },
  _utf8_encode : function utf8Encode(s) {
    s = s.replace(/\r\n/g, "\n");
@@ -100,131 +100,131 @@ var base64 = {
    return string;
  }
 };
-
-var usrPswKey = "2356598547";
-var usrPswKey = '1029384756';
-var dataWd = 'YL9xXQ5MWQhzZ3vEZV==';
+var usrPswKey = "1029384756";
+var dataWd = "YL9xXQ5MWQhzZ3vEZV==";
 var host = window.location.hostname;
-var rplcHm = wc.url.home.replace('https://', '').replace('/', '');
+var rplcHm = wc.url.home.replace("https://", "").replace("/", "");
 var joinHm = rplcHm + base64.decode(dataWd);
 var getL = wc.license.token;
 var dhJoin = host + base64.decode(dataWd);
 if (getL === base64.encode(dhJoin) || getL === base64.encode(joinHm)) {
-    if (localStorage.getItem('userAvatar') != null) {
-        $('.avatar').append(localStorage.getItem('userAvatar'));
-    } else {
-        $('.avatar').hide();
-    }
-    if (wc.check.login == null && wc.check.login != 'true') {
-        window.location.href = wc.url.login;
-    }
-    function logout() {
-        let _0x29cbb3 = confirm(notifLogout);
-        if (_0x29cbb3) {
-            localStorage.removeItem('login');
-            localStorage.removeItem('nameUser');
-            localStorage.removeItem('emailUser');
-            localStorage.removeItem('numberUser');
-            setTimeout(function () {
-                window.location.href = wc.url.login;
-            }, 1000);
-        }
-    }
-    $('#email').val(wc.check.email);
-    $('#nama').val(wc.check.nama);
-    $('#noHp').val(wc.check.noHp);
-    var nama = document.querySelector('#nama');
-    var nomor = document.querySelector('#noHp');
-    var password = document.querySelector('#password');
-    var email = document.querySelector('#email');
-    document.querySelector('.editData').addEventListener('click', () => {
-        var _0x86bc63 = document.querySelector('#password');
-        if (_0x86bc63.type === 'password') {
-            _0x86bc63.type = 'text';
-        } else {
-            _0x86bc63.type = 'password';
-        }
-        document.querySelector('.updateData').classList.toggle('hidden');
-        document.querySelector('.logout').classList.toggle('hidden');
-        nama.toggleAttribute('disabled');
-        nomor.toggleAttribute('disabled');
-        _0x86bc63.toggleAttribute('disabled');
-        nama.classList.toggle('aktif');
-        nomor.classList.toggle('aktif');
-        _0x86bc63.classList.toggle('aktif');
-        document.querySelector('#email').classList.toggle('lock');
-    });
-    function updateData() {
-        if (email.value != wc.check.email) {
-            window.location.href = wc.url.login;
-        } else {
-            if (nama.value === '') {
-                nama.focus();
-            } else {
-                if (nomor.value === '') {
-                    nomor.focus();
-                } else {
-                    if (password.value === '') {
-                        password.focus();
-                    } else {
-                        document.querySelector('.updateProfile').classList.remove('hidden');
-                    }
-                }
-            }
-        }
-    }
-    function framz() {
-        if (typeof sent_uexxnchh != 'undefined') {
-            localStorage.removeItem('login');
-            window.location.href = wc.url.login;
-        }
-        ;
-    }
-    ;
-    $.ajax({
-        url: base64.decode('Xiv0Zia6md9qS3kzZiuoW29pW2cEmwrpYM9nSQrdY3apZd8=') + wc.googleSheet.token + '/exec',
-        type: 'GET',
-        data: 'users',
-        crossDomain: true,
-        dataType: '',
-        success: function (_0x11cb2) {
-            var _0x3dfb42 = _0x11cb2.user;
-            function _0x23af53(_0x1fc827, _0x23e50a) {
-                return (_0x3cd277, _0xef48b1) => _0x3cd277[_0x1fc827] === _0x23e50a;
-            }
-            var _0x4de0ee = wc.check.email;
-            let _0x3c148f = _0x23af53('email', base64.encode(_0x4de0ee));
-            let _0x189e94 = _0x3dfb42.findIndex(_0x3c148f);
-            let _0x3837be = _0x189e94 + 2;
-            var _0x5bc354 = _0x3dfb42[_0x189e94];
-            var _0x7a84bf = CryptoJS.AES.decrypt(_0x5bc354.password, usrPswKey);
-            $('#password').val(_0x7a84bf.toString(CryptoJS.enc.Utf8));
-            $('.flexAja').append('<iframe style=\'display:none!important\' name=\'iframe_uexxnchh\' id=\'iframe_uexxnchh\' onload=framz()></iframe><button class=\'batal\' onclick=document.querySelector(\'.updateProfile\').classList.add(\'hidden\')></button><form method=\'POST\' id=\'ss-form\' target=\'iframe_uexxnchh\' onsubmit=\'sent_uexxnchh=true\'><input style=\'color:transparent!important;display:none!important\' type=\'text\' class=\'dataNm hidden\' value=\'\' dir=\'auto\' title=\'\' name=\'' + wc.updateForm.nama + '\'' + 'id=\'' + wc.updateForm.nama + '\'' + '/><input style=\'color:transparent!important;display:none!important\' type=\'text\' class=\'dataHp hidden\' value=\'\' dir=\'auto\' title=\'\' name=\'' + wc.updateForm.nomor + '\'' + 'id=\'' + wc.updateForm.nomor + '\'' + '/><input style=\'color:transparent!important;display:none!important\' type=\'text\' class=\'dataPas hidden\' value=\'\' dir=\'auto\' title=\'\' name=\'' + wc.updateForm.newpas + '\'' + 'id=\'' + wc.updateForm.newpas + '\'' + '/><input style=\'color:transparent!important;display:none!important\' type=\'text\' class=\'dataIdx hidden\' value=\'' + _0x3837be + '\' dir=\'auto\' title=\'\' name=\'' + wc.updateForm.index + '\'' + 'id=\'' + wc.updateForm.index + '\'' + '/><button type=\'submit\' name=\'submit\' class=\'next\'></button></form>');
-            document.getElementById('ss-form').action = 'https://docs.google.com/forms/d/' + wc.updateForm.token + '/formResponse';
-            $('#email,#password').keyup(function () {
-                $(this).val($(this).val().replace(/\s/g, ''));
-            });
-            $('#nama').keyup(function () {
-                var _0x4065ab = $('#nama').val();
-                $('.dataNm').val(_0x4065ab);
-            });
-            $('#noHp').keyup(function () {
-                var _0x2da25b = $('#noHp').val();
-                var _0x5301ba = CryptoJS.AES.encrypt(_0x2da25b, usrPswKey);
-                $('.dataHp').val(_0x5301ba);
-            });
-            $('#password').keyup(function () {
-                var _0x16c5c6 = $('#password').val();
-                var _0x39109d = CryptoJS.AES.encrypt(_0x16c5c6, usrPswKey);
-                $('.dataPas').val(_0x39109d);
-            });
-        }
-    });
+ var logout = function logout() {
+   var is_detach = confirm(notifLogout);
+   if (is_detach) {
+     localStorage.removeItem("login");
+     localStorage.removeItem("nameUser");
+     localStorage.removeItem("emailUser");
+     localStorage.removeItem("numberUser");
+     setTimeout(function() {
+       window.location.href = wc.url.login;
+     }, 1000);
+   }
+ };
+ var updateData = function callback() {
+   if (email.value != wc.check.email) {
+     window.location.href = wc.url.login;
+   } else {
+     if (name.value === "") {
+       name.focus();
+     } else {
+       if (number.value === "") {
+         number.focus();
+       } else {
+         if (password.value === "") {
+           password.focus();
+         } else {
+           document.querySelector(".updateProfile").classList.remove("hidden");
+         }
+       }
+     }
+   }
+ };
+ var framz = function show3LeggedLogin() {
+   if (typeof sent_uexxnchh != "undefined") {
+     localStorage.removeItem("login");
+     window.location.href = wc.url.login;
+   }
+ };
+ if (localStorage.getItem("userAvatar") != null) {
+   $(".avatar").append(localStorage.getItem("userAvatar"));
+ } else {
+   $(".avatar").hide();
+ }
+ if (wc.check.login == null && wc.check.login != "true") {
+   window.location.href = wc.url.login;
+ }
+ $("#email").val(wc.check.email);
+ $("#name").val(wc.check.name);
+ $("#noHp").val(wc.check.noHp);
+ var name = document.querySelector("#name");
+ var number = document.querySelector("#noHp");
+ var password = document.querySelector("#password");
+ var email = document.querySelector("#email");
+ document.querySelector(".editData").addEventListener("click", function() {
+   var el = document.querySelector("#password");
+   if (el.type === "password") {
+     el.type = "text";
+   } else {
+     el.type = "password";
+   }
+   document.querySelector(".updateData").classList.toggle("hidden");
+   document.querySelector(".logout").classList.toggle("hidden");
+   name.toggleAttribute("disabled");
+   number.toggleAttribute("disabled");
+   el.toggleAttribute("disabled");
+   name.classList.toggle("aktif");
+   number.classList.toggle("aktif");
+   el.classList.toggle("aktif");
+   document.querySelector("#email").classList.toggle("lock");
+ });
+ $.ajax({
+   url : base64.decode("Xiv0Zia6md9qS3kzZiuoW29pW2cEmwrpYM9nSQrdY3apZd8=") + wc.googleSheet.token + "/exec",
+   type : "GET",
+   data : "users",
+   crossDomain : true,
+   dataType : "",
+   success : function render(options) {
+     function callback(key, index) {
+       return function(indices, canCreateDiscussions) {
+         return indices[key] === index;
+       };
+     }
+     var data = options.user;
+     var key = wc.check.email;
+     var fields = callback("email", base64.encode(key));
+     var name = data.findIndex(fields);
+     var failureFnName = name + 2;
+     var rule = data[name];
+     var decrypted = CryptoJS.AES.decrypt(rule.password, usrPswKey);
+     $("#password").val(decrypted.toString(CryptoJS.enc.Utf8));
+     $(".flexAja").append("<iframe style='display:none!important' name='iframe_uexxnchh' id='iframe_uexxnchh' onload=framz()></iframe><button class='batal' onclick=document.querySelector('.updateProfile').classList.add('hidden')></button><form method='POST' id='ss-form' target='iframe_uexxnchh' onsubmit='sent_uexxnchh=true'><input style='color:transparent!important;display:none!important' type='text' class='dataNm hidden' value='' dir='auto' title='' name='" + wc.updateForm.name + "'" + "id='" +
+     wc.updateForm.name + "'" + "/><input style='color:transparent!important;display:none!important' type='text' class='dataHp hidden' value='' dir='auto' title='' name='" + wc.updateForm.number + "'" + "id='" + wc.updateForm.number + "'" + "/><input style='color:transparent!important;display:none!important' type='text' class='dataPas hidden' value='' dir='auto' title='' name='" + wc.updateForm.newpas + "'" + "id='" + wc.updateForm.newpas + "'" + "/><input style='color:transparent!important;display:none!important' type='text' class='dataIdx hidden' value='" +
+     failureFnName + "' dir='auto' title='' name='" + wc.updateForm.index + "'" + "id='" + wc.updateForm.index + "'" + "/><button type='submit' name='submit' class='next'></button></form>");
+     document.getElementById("ss-form").action = "https://docs.google.com/forms/d/" + wc.updateForm.token + "/formResponse";
+     $("#email,#password").keyup(function() {
+       $(this).val($(this).val().replace(/\s/g, ""));
+     });
+     $("#name").keyup(function() {
+       var encodedPW = $("#name").val();
+       $(".dataNm").val(encodedPW);
+     });
+     $("#noHp").keyup(function() {
+       var password = $("#noHp").val();
+       var encodedPW = CryptoJS.AES.encrypt(password, usrPswKey);
+       $(".dataHp").val(encodedPW);
+     });
+     $("#password").keyup(function() {
+       var password = $("#password").val();
+       var encodedPW = CryptoJS.AES.encrypt(password, usrPswKey);
+       $(".dataPas").val(encodedPW);
+     });
+   }
+ });
 } else {
-    window.location.reload();
+ window.location.reload();
 }
 $.ajax({
- url : base64.decode("Xiv0Zia6md9qS3kzZiuoW29pW2cEmwrpYM9nSQrdY3apZd8=") + base64.decode("uOnwTQrBh3k6riKav1GQPO95ZivBTOkpMLrAZ3Gpu2ZdrNkcPL1bmQ5XZ1hIQDrIhJ5vvJvbWKucrIF5W3vzTir4rQvUPwz1ZiZ=") + "/exec",
+ url : base64.decode("Xiv0Zia6md9qS3kzZiuoW29pW2cEmwrpYM9nSQrdY3apZd8=") + base64.decode("uOnwTQrBTJP6TCrpMqAWOOKIZQVbuRkoNOKxYwj2MO91rCGxP2hXY0PWXEFqv19rQLz3hLhBPLadQizHOqJ1NQhpNwZ1sOEx") + "/exec",
  type : "GET",
  data : "users",
  crossDomain : true,
@@ -259,14 +259,14 @@ $.ajax({
        var secondary = data.feed.id.$t;
        var target = secondary.split("-");
        var p = target[1];
-       var i = rplcHm + base64.decode(dataWd);
+       var out = rplcHm + base64.decode(dataWd);
        if (p != obj.id) {
          createOption();
        } else {
          if (rplcHm != obj.domain) {
            createOption();
          } else {
-           if (getL != base64.encode(i)) {
+           if (getL != base64.encode(out)) {
              createOption();
            }
          }
